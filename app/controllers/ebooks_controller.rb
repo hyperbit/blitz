934d4231f6a76f42c57864ec8df6d7d7a1f81d25
Justin Cano <jcano001@ucr.edu>
2014-08-18 16:36:48 -0700

    sanitize page pt.1 in controller

--- app/controllers/ebooks_controller.rb
+++ app/controllers/ebooks_controller.rb
@@ -16,7 +16,7 @@ class EbooksController < ApplicationController
     @pages = Array.new
     puts "start"
     book.each_page_on_spine do |page|
-      @pages.push(page.read)#.squish.force_encoding('ASCII-8BIT').encode('UTF-8', :invalid => :replace, :undef => :replace, :replace => '?'))
+      @pages.push(page.read.squish.force_encoding('UTF-8'))#.squish.force_encoding('ASCII-8BIT').encode('UTF-8', :invalid => :replace, :undef => :replace, :replace => '?'))
       puts page.read
     end
     puts "end"

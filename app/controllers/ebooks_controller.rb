1d2165216d87f515b705f82025efbf7e28305296
Justin Cano <jcano001@ucr.edu>
2014-08-19 16:02:17 -0700

    embedded spritz javascript sdk

--- app/controllers/ebooks_controller.rb
+++ app/controllers/ebooks_controller.rb
@@ -6,7 +6,7 @@ class EbooksController < ApplicationController
   def show
     @ebook = Ebook.find(params[:id])
     @pages = @ebook.pages.paginate(:page => params[:page], :per_page => 1)
-    render 'show'
+    render 'show', :layout => false
   end
 
   def new

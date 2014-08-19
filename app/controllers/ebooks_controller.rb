e7dadb035e52b1fafa4d27f8940ca5e7ebac7ad7
Justin Cano <jcano001@ucr.edu>
2014-08-19 13:59:39 -0700

    paginated

--- app/controllers/ebooks_controller.rb
+++ app/controllers/ebooks_controller.rb
@@ -5,7 +5,7 @@ class EbooksController < ApplicationController
 
   def show
     @ebook = Ebook.find(params[:id])
-    @pages = @ebook.pages
+    @pages = @ebook.pages.paginate(:page => params[:page], :per_page => 1)
     render 'show'
   end
 
@@ -15,25 +15,12 @@ class EbooksController < ApplicationController
 
   def create
   	@ebook = Ebook.new(ebook_params)
-
-    puts "YAYYY"
-    
-    @pages = Array.new
-    puts "start"
-    
-    puts "end"
-    #puts pages
-    #render :text => @pages.encode('UTF-8', :invalid => :replace, :undef => :replace)
-    #render :text => ActionView::Base.full_sanitizer.sanitize("#{text}", :tags=>[])
     if @ebook.save
     	book = EPUB::Parser.parse(@ebook.attachment.path)
-  		puts @ebook.attachment.path
     	book.each_page_on_spine do |pg|
     		p = {}
     		p[:content] = pg.read.squish.force_encoding('UTF-8')
     		@page = @ebook.pages.create(p)
-      	@pages.push(pg.read.squish.force_encoding('UTF-8'))#.squish.force_encoding('ASCII-8BIT').encode('UTF-8', :invalid => :replace, :undef => :replace, :replace => '?'))
-      	puts pg.read
     	end
       redirect_to ebooks_path, notice: "The ebook #{@ebook.title} has been uploaded."
     else
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,16 @@
 class PagesController < ApplicationController
+	def show
+    puts "**************"
+    @page = Page.find(params[:ebook_id])
+    #puts params.inspect()
+    #@ebooks = Ebook.paginate(:page => params[:page])
+    #puts @ebook.pages.inspect()
+    #@pages = @ebook.pages.paginate(:page => params[:id])
+    #puts @pages
+    puts "**************"
+    #@pg = Page.all
+    #puts @pg
+    puts "**************"
+    render 'show'
+  end
 end

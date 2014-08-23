bbedca0acf285d73c5c3f6cd19cdd6f45c6f1474
Justin Cano <jcano001@ucr.edu>
2014-08-23 13:56:12 -0700

    added cover page and redirect facebook login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1517142275185874', 'APP-SECRET'
+  provider :facebook, '1517142275185874', 'b69b2474d3c149940a3c08ef83950800'
 end

419ef8244cbe42174c4a87bea3156f1c292325de
Justin Cano <jcano001@ucr.edu>
2014-08-26 14:07:03 -0700

    REFACTOR: added Facebook ID and Secret to environment variable

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1517142275185874', 'b4667e4766a1d18867317ef279812b25'
+  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']
 end

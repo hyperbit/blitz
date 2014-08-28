66659ba86eb7a11ae2ca7d573b8bf3ec1267359d
Justin Cano <jcano001@ucr.edu>
2014-08-28 14:04:19 -0700

    #destroy deletes all files and ActiveRecords

--- app/controllers/ebooks_controller.rb
+++ app/controllers/ebooks_controller.rb
@@ -1,6 +1,6 @@
 class EbooksController < ApplicationController
   def index
-    Ebook.delete_all(["updated_at < ?", 30.seconds.ago])
+    Ebook.delete_all(["updated_at < ?", 12.hours.ago])
     @ebook = Ebook.all
   end
 
@@ -11,7 +11,7 @@ class EbooksController < ApplicationController
   end
 
   def new
-    Ebook.delete_all(["updated_at < ?", 30.seconds.ago])
+    Ebook.delete_all(["updated_at < ?", 12.hours.ago])
   	@ebook = Ebook.new
   end
 
@@ -20,13 +20,14 @@ class EbooksController < ApplicationController
     params[:ebook][:title] = book.metadata.title
   	@ebook = Ebook.new(ebook_params)
     if @ebook.save
-      destination = "public/uploads/ebook/#{@ebook.title.tr(' ', '_')}"
+      destination = "public/uploads/ebooks/#{@ebook.title}"
       unzip(@ebook.attachment.path, destination)
 
     	book.each_page_on_spine do |pg|
         doc = Nokogiri::HTML(pg.read.squish.force_encoding('UTF-8'))
         body = doc.xpath('//body')
         path = File.join(destination, pg.entry_name)
+        path.slice! "public/"
     		p = {}
     		p[:content] = body.to_s
         p[:path] = path
@@ -39,10 +40,9 @@ class EbooksController < ApplicationController
   end
 
   def destroy
-  	@ebook = Ebook.find(params[:id])
-    dir = ::Rails.root.join("tmp", @ebook.title)
+    dir = "public/uploads/ebooks"
     FileUtils.rm_rf(dir)
-    @ebook.destroy
+    Ebook.delete_all
   	redirect_to ebooks_path, notice: "Ebook deleted!"
   end
 	private
--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -13,7 +13,7 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}"
+    "uploads/#{model.class.to_s.underscore}s"
   end
 
   def cache_dir

4275602563b00aebc5a0df56daf09c7ed10fde70
Justin Cano <jcano001@ucr.edu>
2014-09-02 01:16:54 -0700

    changed route to new ebook

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   resources :ebooks, only: [:index, :new, :create, :destroy, :show] do
     resources :pages
   end
-  root "ebooks#index"
+  root "ebooks#new"
 end
